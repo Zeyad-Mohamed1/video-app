b819c42efd72e1936bb73489cf887e13b2d5849b
Zeyad-Sharawy <125893630+Zeyad-Mohamed1@users.noreply.github.com>
2024-01-22 21:31:08 +0200

    Initial commit

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://video-api-five.vercel.app/api",
+    baseURL: "https://fabulous-croissant-959d76.netlify.app/api",
     withCredentials: true,
 });
